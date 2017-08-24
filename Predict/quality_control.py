fb9f5a34e29a35f2f311f19d928b415b9f341a5f
Mattia Bosio <mattia.bosio85@gmail.com>
2017-08-24 21:25:09 +0200

    Update quality_control.py

--- Predict/quality_control.py
+++ Predict/quality_control.py
@@ -4,7 +4,7 @@ def run_fastqc(read1, read2,fastqc,output):
     print Cmd
     subprocess.call(Cmd,shell=True)
     results = []
-    for dName, sdName, fList in os.walk(inDIR):
+    for dName, sdName, fList in os.walk(output):
          for fileName in fList:
                 if fileName.endswith(fastqc_data.txt):
                     results.append(os.path.join(dName, fileName))
